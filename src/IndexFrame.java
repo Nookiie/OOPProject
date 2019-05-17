f0722cff5ada9610c05acc5f78e3b53d1ea2a44d
Viktor Ivanov <33068739+Nookiie@users.noreply.github.com>
2019-05-17 12:41:57 +0300

    Made some changes to the Index Frame

--- src/IndexFrame.java
+++ src/IndexFrame.java
@@ -1,3 +1,4 @@
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,6 +9,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class IndexFrame extends JFrame
 {
@@ -16,12 +20,22 @@ public class IndexFrame extends JFrame
 	
 	JLabel introLabel = new JLabel("Welcome to the Site Control Panel");
 	
+	JPanel upPanel = new JPanel();
 	JPanel midPanel = new JPanel();
 	JPanel downPanel = new JPanel();
 	
 	JButton companiesButton = new JButton("Companies");
 	JButton categoriesButton = new JButton("Categories");
 	JButton videoGamesButton = new JButton("VideoGames");
+	
+	JButton filterButtonCompany = new JButton("Filter by Company");
+	JButton filterButtonCategory= new JButton("Filter by Category");
+	
+	JTextField companyInput = new JTextField();
+	JTextField categoryInput = new JTextField();
+	
+	JTable sqlTable = new JTable();
+	JScrollPane scrollPane = new JScrollPane(sqlTable);
 	
 	VideoGameFrame videoGameTab = new VideoGameFrame();
 	CategoryFrame categoryTab = new CategoryFrame();
@@ -34,12 +48,28 @@ public class IndexFrame extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(700, 400);
 		this.setLocation(450, 200);
-		this.setLayout(new GridLayout(4, 1));
-	
-		this.add(introLabel);
-		this.add(categoriesButton);
-		this.add(videoGamesButton);
-		this.add(companiesButton);
+		this.setLayout(new GridLayout(4,2));
+			
+		this.add(upPanel);
+		this.add(midPanel);
+		this.add(downPanel);
+
+		upPanel.add(introLabel);
+		midPanel.setLayout(new GridLayout(1,6));
+		midPanel.add(categoriesButton);
+		midPanel.add(videoGamesButton);
+		midPanel.add(companiesButton);
+		
+		companyInput.setColumns(12);
+		categoryInput.setColumns(12);
+		
+		downPanel.setLayout(new FlowLayout());
+		downPanel.add(companyInput);
+		downPanel.add(filterButtonCompany);
+		downPanel.add(categoryInput);
+		downPanel.add(filterButtonCategory);
+		
+		this.add(scrollPane);
 		
 		categoriesButton.addActionListener(new CategoryView());
 		videoGamesButton.addActionListener(new VideoGameView());
